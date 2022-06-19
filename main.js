fadfaa49fa5223fb5f89c26f156e41afc28a30e3
Hazem-Farhati <79176361+Hazem-Farhati@users.noreply.github.com>
2022-06-19 21:12:35 +0100

    mazlt ma kamltech

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ let products = [
     inShop :  0 
    },
    {
-    name: 'SMART TV' ,
+    name: 'smartv' ,
     prix: 500 ,
     inShop :  0 
 
    },
    {
-    name: 'machine a laver' ,
+    name: 'machin' ,
     prix: 900 ,
     inShop :  0 
 
@@ -25,6 +25,7 @@ let products = [
 for(let i=0; i<shops.length;i++){
     shops[i].addEventListener('click',() => {
         shopNumbers(products[i]);
+        totalCost(products[i])
     })
 }
     function onLoadShopNumbers(){
@@ -53,7 +54,12 @@ setItem(product);
         let shopItems= localStorage.getItem('productsInShop');
         shopItems = JSON.parse(shopItems);
         if(shopItems != null){
-            shopItems[product.name].inShop += 1;
+            if(shopItems[product.name] == undefined){
+                shopItems = {
+                    ...shopItems,
+                    [product.name]:product
+                }
+            }
         }
         else {
             product.inShop = 1 ; 
@@ -61,13 +67,80 @@ setItem(product);
                 [product.name]: product
             }
         }
-        console.log("my shopItems are",shopItems);
-        product.inShop = 1 ;
-         shopItems = {
-            [product.name]: product
-        }
+      
         localStorage.setItem("productsInShop", JSON.stringify
         (shopItems));
     }
+
+    function totalCost(product){
+        // console.log("the procut price is ",product.prix);
+        localStorage.setItem("totalCost",product.prix);
+        var shopCost= localStorage.getItem('totalCost');
+        console.log("my shopCost is ",shopCost);
+        console.log(typeof shopCost)
+        if(shopCost != null){
+            shopCost = parseInt(shopCost);
+            localStorage.setItem("totalCost",shopCost + product.prix);
+        }
+        else{
+            localStorage.setItem("totalCost",product.prix);
+        }
+    }
+
+    function displayShop(){
+        var shopItems = localStorage.getItem("productsInShop");
+        var shopCost= localStorage.getItem('totalCost');
+
+        shopItems = JSON.parse(shopItems);
+        var procutContainer = document.querySelector
+        (".product");
+        console.log(shopItems);
+
+        if (shopItems && procutContainer){
+            procutContainer.innerHTML = '' ;
+            Object.values(shopItems).map(item => {
+                procutContainer.innerHTML += `
+                <div class="product">
+            <img src="./image/${item.name}.jpg">
+            <ion-icon name="close-circle-outline"></ion-icon>
+            
+            <div class="price"> ${item.prix},00</div>
+            <div class="quantity">
+            <ion-icon name="caret-back-outline"></ion-icon>
+            <span>${item.inShop}</span>
+            <ion-icon name="caret-forward-outline"></ion-icon>
+            </div>
+
+                <div class="total">
+                ${item.inShop * item.prix},00
+                </div>
+                `;
+               
+                
+            });
+            procutContainer.innerHTML += `
+            <div class="totale_container">
+            <h4 class="basket_totaltitle">
+            basket total </h4>
+            <h4 class="baskettotal">
+            $${shopCost},00
+            </h4>
+            
+            `
+
+        }
+    }
+
+    var el = document.querySelectorAll('ion-icon');
+
+ 
+
+      function changeColor() {
+        el.style.color = 'red';
+    }
+    el.addEventListener('click',changeColor());
+
+    displayShop();
     onLoadShopNumbers();
+  
 
